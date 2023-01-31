f146743d2d3a33dd2956feb9feb5131d9a56c2c9
Andy Woodward <andy.woodward1967@gmail.com>
2023-01-31 16:31:04 +0000

    Create a custom progress slider that reduces with the time left

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const slider = document.querySelector('#slider')
+const sliderFill = document.querySelector('.fill')
 const timeLeftText = document.querySelector('#time-left')
 const startCount = 5
 let timeLeft = startCount
@@ -6,6 +6,7 @@ let timeLeft = startCount
 const timerId = setInterval(() => {
   timeLeft--
   timeLeftText.textContent = timeLeft
+  sliderFill.style.width = (timeLeft / startCount) * 100 + '%'
   if (timeLeft <= 0) {
     clearInterval(timerId)
     timeLeftText.textContent = 'FIN!'
